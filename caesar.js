0efe5cb37f58c688a92de89f65f770885bd0642b
aresous <aresous@gmail.com>
2021-06-19 12:54:09 -0400

    refactor

--- caesar.js
+++ caesar.js
@@ -2,11 +2,10 @@
 const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 function isUpperCase(letter){
-  if(letter === letter.toUpperCase()){
-    return true
-  } else {
-    return false
-  }
+    const 
+    upper = letter.toUpperCase()
+    return( letter === upper ? true : false)
+
 }
 
 function caesar(message,shift){
